dbbd00568e59c6a6caa0399851a7e0ff1f3b7d1b
seanlam97 <seanlam97@gmail.com>
2024-03-21 23:47:27 -0700

    Simplify FDTD test component

--- gplugins/lumerical/tests/test_fdtd_recipe.py
+++ gplugins/lumerical/tests/test_fdtd_recipe.py
@@ -6,9 +6,7 @@ from pathlib import Path
 
 
 def test_fdtd_recipe():
-    c = gf.Component("two_references")
-    wr1 = c << gf.components.straight(width=0.6, length=1.0, layer=LAYER.WG)
-    c.add_ports(wr1.get_ports_list(), prefix="bot_")
+    c = gf.components.straight(width=0.6, length=1.0, layer=LAYER.WG)
 
     recipe = FdtdRecipe(c, dirpath=Path(__file__).resolve().parent / "test_runs")
     recipe.eval()
