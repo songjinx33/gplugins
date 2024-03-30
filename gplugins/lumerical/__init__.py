d1431279b604bbedfbcd8e56b72c6d8195a7149c
seanlam97 <seanlam97@gmail.com>
2024-03-29 17:58:57 -0700

    [LUM] Remove old CHARGE setup scripts

--- gplugins/lumerical/__init__.py
+++ gplugins/lumerical/__init__.py
@@ -2,9 +2,6 @@ from __future__ import annotations
 
 from gplugins.lumerical.interconnect import run_wavelength_sweep
 from gplugins.lumerical.read import read_sparameters_lumerical
-from gplugins.lumerical.write_charge_distribution_lumerical import (
-    write_charge_distribution_lumerical,
-)
 from gplugins.lumerical.write_sparameters_lumerical import (
     write_sparameters_lumerical,
 )
@@ -15,7 +12,6 @@ from gplugins.lumerical.write_sparameters_lumerical_components import (
 __all__ = [
     "read_sparameters_lumerical",
     "write_sparameters_lumerical",
-    "write_charge_distribution_lumerical",
     "write_sparameters_lumerical_components",
     "run_wavelength_sweep",
 ]
