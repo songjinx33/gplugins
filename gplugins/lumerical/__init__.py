8d54d390aa1307ce6575edfee1288934b9b9d678
seanlam97 <seanlam97@gmail.com>
2024-03-29 22:49:08 -0700

    Remove FDTD and CHARGE setup scripts

--- gplugins/lumerical/__init__.py
+++ gplugins/lumerical/__init__.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
 
 from gplugins.lumerical.interconnect import run_wavelength_sweep
-from gplugins.lumerical.Lumerical import Lumerical
 from gplugins.lumerical.read import read_sparameters_lumerical
-from gplugins.lumerical.write_charge_distribution_lumerical import (
-    write_charge_distribution_lumerical,
-)
 from gplugins.lumerical.write_sparameters_lumerical import (
     write_sparameters_lumerical,
 )
@@ -16,8 +12,6 @@ from gplugins.lumerical.write_sparameters_lumerical_components import (
 __all__ = [
     "read_sparameters_lumerical",
     "write_sparameters_lumerical",
-    "write_charge_distribution_lumerical",
     "write_sparameters_lumerical_components",
     "run_wavelength_sweep",
-    "Lumerical",
 ]
